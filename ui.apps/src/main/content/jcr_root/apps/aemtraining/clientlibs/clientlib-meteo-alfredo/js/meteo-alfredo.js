d83bd297fcb1f8a8430d755be2acc33eebdf00df
alfredofiore <alfredofiore.work@gmail.com>
2021-10-06 10:56:56 +0200

    aggiunta chiamata jquery alfredo

--- ui.apps/src/main/content/jcr_root/apps/aemtraining/clientlibs/clientlib-meteo-alfredo/js/meteo-alfredo.js
+++ ui.apps/src/main/content/jcr_root/apps/aemtraining/clientlibs/clientlib-meteo-alfredo/js/meteo-alfredo.js
@@ -1,17 +1,20 @@
+// chiamata ajax jQuery
+console.log("meteoalfredo ajax richiamamta");
+$(document).ready(function() {
+    $.ajax({
 
-// Esempio POST
-async function getData(url) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'GET', 
-  //  mode: 'cors', // no-cors, *cors, same-origin
-   // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-  //  credentials: 'same-origin', // include, *same-origin, omit
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
+        url:'/content/aemtraining/us/en/meteoalfredo/jcr:content/root/container/container/meteo_alfredo.json',
+        // ovviamente questo url con javaScript possiamo calcolarlo con una funzione automatica
+        dataType: "json",
+        success: function(result) {
+            var htmlTemplate = $("#meteoTemplate").html();
+            var template = Handlebars.compile(htmlTemplate);
+            $("#meteoContainer").html(template(result)); //imposta il risultato nel conteiner con id 'meteoContainer'
+            console.log("risultato =  ", result);
+        },
+        error: function(error) {
+            console.log("errore =  ", error);
 
-getData('https://api.openweathermap.org/data/2.5/onecall?lat=39.2953&lon=16.2536&exclude=current,minutely,hourly,alerts&units=metric&&appid=fd4a3d5871e3824ca6748d29aa1b25d8')
-  .then(data => {
-    console.log(data); // JSON data parsed by `data.json()` call
-  });
+        }
+    });
+});
