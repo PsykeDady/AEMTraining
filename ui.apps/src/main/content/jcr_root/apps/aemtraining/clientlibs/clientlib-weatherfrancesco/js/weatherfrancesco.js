14eccc5ee8ff120db342b53c3ed654e9315370c9
FCosca <francecosca@gmail.com>
2021-10-06 10:18:34 +0200

    upgrade weather

--- ui.apps/src/main/content/jcr_root/apps/aemtraining/clientlibs/clientlib-weatherfrancesco/js/weatherfrancesco.js
+++ ui.apps/src/main/content/jcr_root/apps/aemtraining/clientlibs/clientlib-weatherfrancesco/js/weatherfrancesco.js
@@ -1,8 +1,13 @@
 console.log("WeatherFraAjax");
+const myString = window.location.pathname
+const StringtoSplit = myString.split(".")[0]
+const finalurl=StringtoSplit+"/jcr:content/root/container/container/weatherfrancesco.json"
+
+console.log("url della pagina e'=",finalurl)
 
 $(document).ready(function(){
     $.ajax({
-        url: `/content/aemtraining/us/en/bianca/jcr:content/root/container/container/weatherfrancesco.json`,
+        url:finalurl ,
         dataType: "json",
         success: function(result){
             var htmlTemplate = $("#weatherTemplate").html();
