fe6bd8e1cbd34548720dbb8697f4ebfc11419753
PsykeDady <psdady@msn.com>
2021-10-06 10:27:15 +0200

    edit meteo davide

--- ui.apps/src/main/content/jcr_root/apps/aemtraining/clientlibs/clientlib-meteo-davide/js/meteo.js
+++ ui.apps/src/main/content/jcr_root/apps/aemtraining/clientlibs/clientlib-meteo-davide/js/meteo.js
@@ -1,7 +1,12 @@
 console.log("WeatherDavAjax");
+var percorso = window.location.pathname
+percorso = percorso.split(".")[0]
+percorso=percorso+"/jcr:content/root/container/container/weatherdavide.json"
+
+console.log("percorso=",percorso);
 $(document).ready(function(){
     $.ajax({
-        url: `jcr:weatherDavide.json`,
+        url: percorso,
         dataType: "json",
         success: function(result){
             var htmlTemplate = $("#weatherTemplate").html();
