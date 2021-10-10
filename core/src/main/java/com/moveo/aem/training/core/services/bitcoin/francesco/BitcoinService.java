07695c5d507ebcebf4c15cd884453172d20accd6
FCosca <francecosca@gmail.com>
2021-10-10 20:16:41 +0200

    Bitcoin Francesco

---
+++ core/src/main/java/com/moveo/aem/training/core/services/bitcoin/francesco/BitcoinService.java
@@ -0,0 +1,8 @@
+package com.moveo.aem.training.core.services.bitcoin.francesco;
+
+import com.moveo.aem.training.core.beans.bitcoin.francesco.BitcoinBean;
+
+public interface BitcoinService {
+
+    public BitcoinBean getBitcoin() throws Exception;
+}
